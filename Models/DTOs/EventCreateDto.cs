9cf4a154ada9432eabc191eb3774790acc026f3a
SanteR1 <thesanter88@gmail.com>
2026-04-06 22:15:40 +0500

    Обновил правила валидации входящих параметров

--- Models/DTOs/EventCreateDto.cs
+++ Models/DTOs/EventCreateDto.cs
@@ -4,15 +4,11 @@ namespace CoreEvents.Models.DTOs
 {
     public record EventCreateDto(
         [Required(AllowEmptyStrings = false, ErrorMessage = "Название обязательно")]
-        [StringLength(100, MinimumLength = 2,
-            ErrorMessage = "Название должно быть от 2 до 100 символов")]
         string Title,
         string? Description,
         [Required(ErrorMessage = "Дата начала обязательна")]
-        [Range(typeof(DateTime), "2020-01-01", "2030-12-31", ErrorMessage = "Дата начала вне диапазона")]
         DateTime StartAt,
         [Required(ErrorMessage = "Дата окончания обязательна")]
-        [Range(typeof(DateTime), "2020-01-01", "2030-12-31", ErrorMessage = "Дата окончания вне диапазона")]
         DateTime EndAt
     );
 }
