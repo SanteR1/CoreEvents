578ab185d4263c9809a44de2b0eaa41467010b43
SanteR1 <thesanter88@gmail.com>
2026-08-07 17:51:20 +0500

    Users.Api

---
+++ Services/Users/Users.Api/DependencyInjection.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi;
+using Users.Api.ExceptionHandlers;
+using Users.Api.Services;
+using Users.Application.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentationServices(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUserContext, UserContext>();
+
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+        services.AddProblemDetails();
+
+        services.AddExceptionHandler<DomainExceptionHandler>();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        services.AddOpenApi();
+        services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+            {
+                Description = "Введите JWT токен",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                BearerFormat = "JWT"
+            });
+            option.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecuritySchemeReference(JwtBearerDefaults.AuthenticationScheme, document)] = []
+            });
+        });
+
+        return services;
+    }
+}
