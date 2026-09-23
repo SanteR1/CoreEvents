7113f9608d53b31e4f723c5da0febca5e13f1f53
SanteR1 <thesanter88@gmail.com>
2026-09-23 17:47:44 +0000

    fix(gateway): нормализация имени спана и http.route в трейсах Jaeger

--- Gateway/Gateway.Api/Extensions/OpenTelemetryExtensions.cs
+++ Gateway/Gateway.Api/Extensions/OpenTelemetryExtensions.cs
@@ -1,12 +1,17 @@
+using System.Text.RegularExpressions;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Yarp.ReverseProxy.Model;
 
 namespace Gateway.Api.Extensions;
 
-public static class OpenTelemetryExtensions
+public static partial class OpenTelemetryExtensions
 {
+    [GeneratedRegex(@"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")]
+    private static partial Regex GuidRegex();
+
     public static IServiceCollection AddGatewayTelemetry(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddOpenTelemetry()
@@ -21,6 +26,25 @@ public static class OpenTelemetryExtensions
                         return !path.StartsWithSegments("/metrics")
                             && !path.StartsWithSegments("/swagger")
                             && !path.StartsWithSegments("/docs");
+                    };
+                    options.EnrichWithHttpResponse = (activity, response) =>
+                    {
+                        var req = response.HttpContext.Request;
+                        var rawPath = req.Path.Value ?? "";
+                        var cleanRoute = GuidRegex().Replace(rawPath, "{id}");
+
+                        activity.DisplayName = $"{req.Method} {cleanRoute}";
+                        activity.SetTag("http.route", cleanRoute);
+
+                        var proxyFeature = response.HttpContext.GetReverseProxyFeature();
+                        if (proxyFeature?.Route?.Config?.RouteId is { } routeId)
+                        {
+                            activity.SetTag("gateway.route_id", routeId);
+                        }
+                        if (proxyFeature?.ProxiedDestination?.Model?.Config?.Address is { } destAddress)
+                        {
+                            activity.SetTag("gateway.destination", destAddress);
+                        }
                     };
                 })
                 .AddHttpClientInstrumentation()
